5ac379ed75215a32aa2ee1b84c69b53dd884add3
AndyGitNix <sigsint@proton.me>
2025-01-11 12:07:34 +0200

    Added check for need to run foo.clean-command for kismet-journal files. Added feature to combine the exported csv files into one.

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 This Python script is meant to be used with Kismet and its built-in tool kismetdb_to_wiglecsv to export .kismet-files to csv-format. Script will go through all the files in the directory and export the .kismet files as .csv for further processing or for example uploading to wigle.net.
 
-Change the directory in the script to where you have your .kismet-files saved:
+Requirements:
+```
+pip install pandas
+```
+
+Change the directory in the script to where you have your .kismet-files saved (absolute path):
 ```
 os.chdir('PATH/TO/DIRECTORY')
 ```
--- kismet_to_csv.py
+++ kismet_to_csv.py
@@ -1,9 +1,10 @@
 # Author: AndyGitNix
-# Version: 0.1
+# Version: 0.2
 
 import os
+import pandas as pd
 
-os.chdir('PATH/TO/DIRECTORY')
+os.chdir('/PATH/TO/FOLDER')                     # Path to where .kismet log-files are stored
 number = 1
 file_exists = True
 csv_name = ''
@@ -24,16 +25,32 @@ def main(file_parts):
             quit()
         return csv_name
 
+
+# Chekcs if folder has '.kismet-journal' files and cleans them if there is.
+
+def clean_check(file_parts):
+    for f_name, f_ext in file_parts:
+        if f_ext == '.kismet-journal':
+            clean_command = 'kismetdb_clean foo.kismet --in {}.kismet'.format(f_name)
+            print('Cleaning file {}.kismet'.format(f_name) + ' with command: kismetdb_clean foo.kismet --in {}.kismet'.format(f_name))
+            os.system(clean_command)
+
+
+# Check if csv-files exist in destination folder and asks for input if files exist.
+
 def csv_check(file_parts):
 
     for _, f_ext in file_parts:
 
         if f_ext == '.csv':
-            csv_exists = input('There is previous .csv-files in your directory. Do you want to continue anyway? Yes/No? [y/n] ')
+            csv_exists = input('There is previous .csv-files in your directory. They will be combined to the finished file.\nDo you want to continue anyway? Yes/No? [y/n] ')
 
             if csv_exists.lower().strip() != 'y':
                 quit()
             break
+
+
+# Run kismetdb_to_wiglecsv command
 
 def to_csv(file_parts, file_exists, number, csv_name):
 
@@ -41,18 +58,50 @@ def to_csv(file_parts, file_exists, number, csv_name):
 
         if f_ext == '.kismet':
             while (file_exists == True):
-                if os.path.exists('{}{}.csv'.format(csv_name, number)):
-                    print('File named {}{}.csv already exists. Renaming...'.format(csv_name, number))
+                if os.path.exists('{}_{}.csv'.format(csv_name, number)):
+                    print('File named {}_{}.csv already exists. Renaming...'.format(csv_name, number))
                     number += 1
                 else:
                     file_exists = False
 
-            command = 'kismetdb_to_wiglecsv --in "{}{}" --out "{}{}.csv"'.format(f_name, f_ext, csv_name, number)
+            command = 'kismetdb_to_wiglecsv --in {}{} --out {}_{}.csv'.format(f_name, f_ext, csv_name, number)
             print('Executing command: ' + command)
             os.system(command)
             number += 1
 
+
+# Combine the exported csv-files into one
+
+def combine_csv(csv_name):
+
+    print('Combining csv files...')
+
+    header = "WigleWifi-1.4,appRelease=Kismet2024120,model=Kismet,release=2024.12.0.8,device=kismet,display=kismet,board=kismet,brand=kismet"
+
+    dataframes = []
+
+    for file in os.listdir(os.getcwd()):
+        if file.endswith('.csv'):
+            df = pd.read_csv(file, skiprows=1, header=0)
+            dataframes.append(df)
+
+    master_df = pd.concat(dataframes, ignore_index=True)
+
+    master_df.to_csv(f'{csv_name}.csv', index=False)
+
+    with open('{}.csv'.format(csv_name), 'r+') as file:
+        content = file.read()
+        file.seek(0, 0)
+        file.write(f"{header}\n{content}")
+
+    print(f"Combined CSV saved as {csv_name}.csv")
+
+
+# def remove_csv():
+
 if __name__ == '__main__':
     csv_name = main(file_parts)
+    clean_check(file_parts)
     csv_check(file_parts)
     to_csv(file_parts, file_exists, number, csv_name)
+    combine_csv(csv_name)
